f203a019dff7a3f2ff281c313b9b25f52f59b860
Sebastiano Smaniotto <ssmaniot@gmail.com>
2021-06-11 14:11:01 +0200

    .gitignore now ignores the "hits" executable

--- makefile
+++ makefile
@@ -2,11 +2,19 @@ CC := gcc
 CFLAGS := -std=gnu89 -Wall -pedantic -O3
 LIBS := -lm
 
+all: pagerank hits 
+
 pagerank: pagerank.o
 	$(CC) -o pagerank pagerank.o $(CFLAGS) $(LIBS)
 
+hits: hits.o
+	$(CC) -o hits hits.o $(CFLAGS) $(LIBS)
+
 pagerank.o: src/pagerank.c 
 	$(CC) -c src/pagerank.c $(CFLAGS)
+
+hits.o: src/hits.c 
+	$(CC) -c src/hits.c $(CFLAGS)
 
 clean:
 	rm -f *.o
--- src/hits.c
+++ src/hits.c
@@ -1,22 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define DEBUG 
+#include <float.h>
+#include <math.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+#define TOL 1.e-10
+#define MAX_ITER 100
+#define FNAME 256
+#define DNAME 1024
+#define PATH 1024
+#define MMAP 2048
+#define DEBUG
 
 /* Data for compression */
 typedef struct 
 {
     int no_nodes;
     int no_edges;
+    int no_danglings;
 } CSR_data;
+
+/* Helper functions */
+int write_data(char path[], void *data, size_t nmemb, size_t size);
+void delete_folder(char dir[]);
+void * mmap_data(char path[], size_t nmemb, size_t size);
+void print_p(double *p, int n);
+void double_merge(int *from, int *to, int lo, int mid, int hi);
+void double_merge_sort(int *from, int *to, int lo, int hi);
+void sort_input_data(int *from, int *to, int n);
 
 int main(int argc, char *argv[])
 {
     /* Data to save/load CSR matrix */
     FILE *pdata;
     char dir[DNAME];
-    char row_ptr_p[2][PATH];
-    char col_ind_p[2][PATH];
-    char csr_data_p[PATH];
+    char row_ptr_p[PATH],  row_ptr_tp[PATH];
+    char col_ind_p[PATH],  col_ind_tp[PATH];
+    char val_p[PATH],      val_tp[PATH];
+    char danglings_p[PATH];
+    char csr_data_p[PATH], csr_data_tp[PATH];
     CSR_data csr_data;
     struct stat st = {0};
     
@@ -37,6 +65,8 @@ int main(int argc, char *argv[])
     int *row_ptr;
     
     /* Pagerank computation data */
+    int *danglings;
+    int no_danglings;
     double danglings_dot_product;
     double *p, *p_new;
     double d;
@@ -44,6 +74,7 @@ int main(int argc, char *argv[])
     int iter;
     
     /* Extra data */
+    int *out_links;
     double sum;
     int err;
     
@@ -54,11 +85,12 @@ int main(int argc, char *argv[])
     }
     
     /* Check if input data has already been compressed */
-    strcpy(dir, "PR_");
-    strncpy(dir + 3, argv[1] + 5, strlen(argv[1])-9);
-    dir[strlen(argv[1])-6] = '/';
-    dir[strlen(argv[1])-5] = '\0';
+    strcpy(dir, "HITS_");
+    strncpy(dir + 5, argv[1] + 5, strlen(argv[1])-9);
+    dir[strlen(argv[1])-4] = '/';
+    dir[strlen(argv[1])-3] = '\0';
     printf("%s\n", dir);
+    exit(EXIT_SUCCESS);
     
     /* Create CSR file names */
     strcpy(row_ptr_p, dir);
@@ -347,4 +379,139 @@ int main(int argc, char *argv[])
     free(p); free(p_new);
     
     exit(EXIT_SUCCESS);
+}
+
+/* Helper functions */
+
+int write_data(char path[], void *data, size_t nmemb, size_t size)
+{
+    FILE *pdata;
+    
+    if ((pdata = fopen(path, "wb")) == NULL)
+    {
+        fprintf(stderr, " [ERROR] Cannot create file \"%s\"\n", path);
+        return EXIT_FAILURE;
+    }
+    fwrite(data, size, nmemb, pdata);
+    fclose(pdata);
+    return EXIT_SUCCESS;
+}
+
+void delete_folder(char dir[])
+{
+    DIR *pf = opendir(dir);
+	struct dirent *next_file;
+	char fpath[PATH];
+	
+	while ((next_file = readdir(pf)) != NULL)
+	{
+		sprintf(fpath, "%s/%s", dir, next_file->d_name);
+		remove(fpath);
+	}
+	closedir(pf);
+	rmdir(dir);
+}
+
+void * mmap_data(char path[], size_t nmemb, size_t size)
+{
+    int fd;
+    char mmap_p[MMAP];
+    void * mp;
+    sprintf(mmap_p, "./%s", path);
+#ifdef DEBUG 
+    printf("mmapping \"%s\"\n", mmap_p);
+#endif 
+    fd = open(mmap_p, O_RDONLY);
+    mp = mmap(NULL, nmemb * size, PROT_READ, MAP_SHARED, fd, 0);
+    if (mp == MAP_FAILED)
+        mp = NULL;
+    close(fd);
+    return mp;
+}
+
+void print_p(double *p, int n)
+{
+    int i;
+    printf("p: [ ");
+    for (i = 0; i < n; ++i)
+        printf("%.3f ", p[i]);
+    printf("]\n");
+}
+
+void double_merge(int *from, int *to, int lo, int mid, int hi)
+{
+    int *FL, *TL; 
+    int *FR, *TR;
+    int i, j;
+    int NL, NR;
+    
+    NL = mid - lo;
+    NR = hi - mid;
+    
+    FL = (int *) malloc(sizeof(int) * NL);
+    TL = (int *) malloc(sizeof(int) * NL);
+    FR = (int *) malloc(sizeof(int) * NR);
+    TR = (int *) malloc(sizeof(int) * NR);
+    
+    for (i = 0; i < NL; ++i)
+    {
+        FL[i] = from[lo+i];
+        TL[i] = to[lo+i];
+    }
+    
+    for (j = 0; j < NR; ++j)
+    {
+        FR[j] = from[mid+j];
+        TR[j] = to[mid+j];
+    }
+    
+    i = 0; j = 0;
+    while (i < NL && j < NR)
+    {
+        if (TL[i] < TR[j] || (TL[i] == TR[j] && FL[i] <= FR[j]))
+        {
+            from[lo+i+j] = FL[i];
+            to[lo+i+j] = TL[i];
+            ++i;
+        }
+        else 
+        {
+            from[lo+i+j] = FR[j];
+            to[lo+i+j] = TR[j];
+            ++j;
+        }
+    }
+    
+    while (i < NL)
+    {
+        from[lo+i+j] = FL[i];
+        to[lo+i+j] = TL[i];
+        ++i;
+    }
+    
+    while (j < NR)
+    {
+        from[lo+i+j] = FR[j];
+        to[lo+i+j] = TR[j];
+        ++j;
+    }
+    
+    free(FL); free(TL);
+    free(FR); free(TR);
+}
+
+void double_merge_sort(int *from, int *to, int lo, int hi)
+{
+    if (hi - lo > 1)
+    {
+        int mid = (lo + hi) / 2;
+        double_merge_sort(from, to, lo, mid);
+        double_merge_sort(from, to, mid, hi);
+        double_merge(from, to, lo, mid, hi);
+    }
+}
+
+void sort_input_data(int *from, int *to, int n)
+{
+    double_merge_sort(from, to, 0, n);
 }
